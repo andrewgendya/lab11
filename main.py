7f570af4c1c16174c1a7e68e48f54e74c31ab0f8
Andrew Gendy <andrewgendyawad@gmail.com>
2025-04-17 13:08:43 -0400

    updated

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import os
+import matplotlib.pyplot as plt
 def print_menu():
     print ("1. Student grade")
-    print ("2. Assignnent statistics")
-    print ("3. Assignnent graph\n")
+    print ("2. Assignment statistics")
+    print ("3. Assignment graph\n")
     return input ("Enter your selection: ")
 def main():
     students= []
     studentIDs = []
-    with open ("data/students.txt", "r") as file:
+    with open("data/students.txt", "r") as file:
         for line in file:
             id = line [0:3]
             name = line [3:]
@@ -20,12 +21,12 @@ def main():
     assignmentNames= []
 
     with open("data/assignments.txt", "r") as file:
-        content = file.read().split ("\n")
-        content. pop(len(content) - 1)
+        content = file.read().split("\n")
+        content.pop(len(content) - 1)
         for i in range(0, len(content), 3):
-            name = content(i)
+            name = content[i]
             ID = content[i+1]
-            weight = int (content [1+2])
+            weight=int(content[i+2])
             assignmentNames.append(name)
             assignmentIDs.append(ID)
             assignmentWeight.append(weight)
@@ -51,14 +52,14 @@ def main():
             studentID = studentIDs[students.index(studentName)]
             maxScore = 0
             studentScore = 0
-        for i in range(0, len(submissionStudent)):
-            if studentID == submissionStudent[i]:
-                score = submissionScore[i]
-                assignmentIndex = assignmentIDs.index(submissionAssignment[i])
-                weight = assignmentWeight[assignmentIndex]
-                maxScore += weight
-                studentScore += int(score) * int(weight)
-        print(studentScore / maxScore)
+            for i in range(0, len(submissionStudent)):
+                if studentID == submissionStudent[i]:
+                    score = submissionScore[i]
+                    assignmentIndex = assignmentIDs.index(submissionAssignment[i])
+                    weight = assignmentWeight[assignmentIndex]
+                    maxScore += weight
+                    studentScore += int(score) * int(weight)
+            print(studentScore / maxScore)
     elif option == "2":
         assignmentName = input("what is the assignment name: ")
         if assignmentName not in assignmentNames:
@@ -68,9 +69,11 @@ def main():
             assignmentScores = []
             for i in range(0, len(submissionAssignment)):
                 if assignmentID == submissionAssignment[i]:
-                    assignmentScores.append(int(submissionScore(i)))
+                    assignmentScores.append(int(submissionScore[i]))
             print ("Min:", min(assignmentScores))
             print ("Avg:", sum(assignmentScores)/len(assignmentScores))
             print ("Max:", max(assignmentScores))
 
 
+if __name__=="__main__":
+    main()
